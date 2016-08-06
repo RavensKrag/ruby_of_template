eb8505dbb82962f8ccb42cb81fce811238e5e04c
Raven <RavensKrag@gmail.com>
2016-08-05 21:18:52 -0400

    load oni.rb which loads oni/oni.so (or platform equivalent)

--- Oni/lib/oni.rb
+++ Oni/lib/oni.rb
@@ -6,7 +6,7 @@ if defined? RUBY_PLATFORM and
 	ENV['PATH'] = "#{File.dirname(__FILE__)};#{ENV['PATH']}"
 end
 
-require 'oni/oni'
+require File.expand_path('oni/oni', File.absolute_path(File.dirname(__FILE__)))
 puts "loading ruby code"
 
 class Numeric
@@ -17,194 +17,221 @@ class Numeric
 	end
 end
 
+
+
+
 module Oni
-	class Window
-		# :kb_capital is CAPS LOCK
-		# :kb_scroll is scroll lock
-		KEY_ID = [:kb_unassigned, :kb_escape,
-			
-			:kb_1, :kb_2, :kb_3, :kb_4, :kb_5, :kb_6, :kb_7, :kb_8, :kb_9, :kb_0, 
-			:kb_minus, :kb_equals, :kb_back, :kb_tab, 
-			:kb_q, :kb_w, :kb_e, :kb_r, :kb_t, :kb_y, :kb_u, :kb_i, :kb_o, :kb_p, :kb_lbracket, :kb_rbracket, :kb_return, 
-			:kb_lcontrol, :kb_a, :kb_s, :kb_d, :kb_f, :kb_g, :kb_h, :kb_j, :kb_k, :kb_l, :kb_semicolon, :kb_apostrophe, :kb_grave, :kb_lshift, :kb_backslash,
-			:kb_z, :kb_x, :kb_c, :kb_v, :kb_b, :kb_n, :kb_m, :kb_comma, :kb_period, :kb_slash, :kb_rshift,
-			
-			:kb_multiply, :kb_lalt, :kb_space, :kb_capital,
-			
-			:kb_f1, :kb_f2, :kb_f3, :kb_f4, :kb_f5, :kb_f6, :kb_f7, :kb_f8, :kb_f9, :kb_f10, :kb_numlock, :kb_scroll, 
-			
-			:kb_numpad7, :kb_numpad8, :kb_numpad9, :kb_subtract, :kb_numpad4, :kb_numpad5, :kb_numpad6, :kb_add, :kb_numpad1, :kb_numpad2, :kb_numpad3, :kb_numpad0, :kb_decimal, 
-			
-			:kb_oem_102,
-			
-			nil, nil, :kb_f11, :kb_f12, :kb_f15,
-			
-			:kb_kana, :kb_abnt_c1, :kb_convert, :kb_noconvert, :kb_yen, :kb_abnt_c2, :kb_numpadequals, :kb_prevtrack, :kb_at, :kb_colon, :kb_underline, :kb_kanji, :kb_stop, :kb_ax, :kb_unlabeled, :kb_nexttrack, :kb_numpadenter, :kb_rcontrol, :kb_mute, :kb_calculator, :kb_playpause, :kb_mediastop, :kb_volumedown, :kb_volumeup, :kb_webhome, :kb_numpadcomma, :kb_divide, :kb_sysrq, :kb_rmenu, :kb_pause, :kb_home, :kb_up, :kb_pgup, :kb_left, :kb_right, :kb_end, :kb_down, :kb_pgdown, :kb_insert, :kb_delete, :kb_lwin, :kb_rwin, :kb_apps, :kb_power, :kb_sleep, :kb_wake, :kb_websearch, :kb_webfavorites, :kb_webrefresh, :kb_webstop, :kb_webforward, :kb_webback, :kb_mycomputer, :kb_mail, :kb_mediaselect,
-			# There are 145 symbols listed above
-			# That's IDs 0-144 inclusive on both ends
-			
-			# 145
-			nil, 
-			# 146
-			nil, 
-			# 147
-			nil, 
-			# 148
-			nil, 
-			# 149
-			nil, 
-			# 150
-			nil, 
-			# 151
-			nil, 
-			# 152
-			nil, 
-			# 153
-			nil, 
-			# 154
-			nil, 
-			# 155
-			nil, 
-			# 156
-			nil, 
-			# 157
-			:rcontrol, #rcontrol
-			# 158
-			nil, 
-			# 159
-			nil, 
-			# 160
-			nil, 
-			# 161
-			nil, 
-			# 162
-			nil, 
-			# 163
-			nil, 
-			# 164
-			nil, 
-			# 165
-			nil, 
-			# 166
-			nil, 
-			# 167
-			nil, 
-			# 168
-			nil, 
-			# 169
-			nil, 
-			# 170
-			nil, 
-			# 171
-			nil, 
-			# 172
-			nil, 
-			# 173
-			nil, 
-			# 174
-			nil, 
-			# 175
-			nil, 
-			# 176
-			nil, 
-			# 177
-			nil, 
-			# 178
-			nil, 
-			# 179
-			nil, 
-			# 180
-			nil, 
-			# 181
-			nil, 
-			# 182
-			nil, 
-			# 183
-			:kb_printscreen, 
-			# 184
-			:ralt, # ralt
-			# 185
-			nil, 
-			# 186
-			nil, 
-			# 187
-			nil, 
-			# 188
-			nil, 
-			# 189
-			nil, 
-			# 190
-			nil, 
-			# 191
-			nil, 
-			# 192
-			nil, 
-			# 193
-			nil, 
-			# 194
-			nil, 
-			# 195
-			nil, 
-			# 196
-			nil, 
-			# 197
-			nil, 
-			# 198
-			nil, 
-			# 199-208
-			:kb_home, :kb_up, :kb_pgup, nil, :kb_left, :kb_end, :kb_right, :kb_pgdown, :kb_end, :kb_down,
-			# 209
-			:kb_pgdown, 
-			# 210
-			:kb_insert,
-			# 211
-			:kb_delete,
-			# 212
-			nil, 
-			# 213
-			nil, 
-			# 214
-			nil, 
-			# 215
-			nil, 
-			# 216
-			nil, 
-			# 217
-			nil, 
-			# 218
-			nil, 
-			# 219
-			:kb_lwin, 
-			# 220
-			:kb_rwin, 
-			# 221
-			:kb_menu
-		]
+
+
+class Window
+	# :kb_capital is CAPS LOCK
+	# :kb_scroll is scroll lock
+	KEY_ID = [:kb_unassigned, :kb_escape,
 		
-		# def update(dt)
-		# end
+		:kb_1, :kb_2, :kb_3, :kb_4, :kb_5, :kb_6, :kb_7, :kb_8, :kb_9, :kb_0, 
+		:kb_minus, :kb_equals, :kb_back, :kb_tab, 
+		:kb_q, :kb_w, :kb_e, :kb_r, :kb_t, :kb_y, :kb_u, :kb_i, :kb_o, :kb_p, :kb_lbracket, :kb_rbracket, :kb_return, 
+		:kb_lcontrol, :kb_a, :kb_s, :kb_d, :kb_f, :kb_g, :kb_h, :kb_j, :kb_k, :kb_l, :kb_semicolon, :kb_apostrophe, :kb_grave, :kb_lshift, :kb_backslash,
+		:kb_z, :kb_x, :kb_c, :kb_v, :kb_b, :kb_n, :kb_m, :kb_comma, :kb_period, :kb_slash, :kb_rshift,
 		
-		# def draw
-		# end
+		:kb_multiply, :kb_lalt, :kb_space, :kb_capital,
 		
-		# def button_down(id)
-			
-		# end
+		:kb_f1, :kb_f2, :kb_f3, :kb_f4, :kb_f5, :kb_f6, :kb_f7, :kb_f8, :kb_f9, :kb_f10, :kb_numlock, :kb_scroll, 
 		
-		# def button_up(id)
-			
-		# end
+		:kb_numpad7, :kb_numpad8, :kb_numpad9, :kb_subtract, :kb_numpad4, :kb_numpad5, :kb_numpad6, :kb_add, :kb_numpad1, :kb_numpad2, :kb_numpad3, :kb_numpad0, :kb_decimal, 
 		
-		# def button_id_to_sym(id)
-		# 	return KEY_ID[id]
-		# end
+		:kb_oem_102,
+		
+		nil, nil, :kb_f11, :kb_f12, :kb_f15,
+		
+		:kb_kana, :kb_abnt_c1, :kb_convert, :kb_noconvert, :kb_yen, :kb_abnt_c2, :kb_numpadequals, :kb_prevtrack, :kb_at, :kb_colon, :kb_underline, :kb_kanji, :kb_stop, :kb_ax, :kb_unlabeled, :kb_nexttrack, :kb_numpadenter, :kb_rcontrol, :kb_mute, :kb_calculator, :kb_playpause, :kb_mediastop, :kb_volumedown, :kb_volumeup, :kb_webhome, :kb_numpadcomma, :kb_divide, :kb_sysrq, :kb_rmenu, :kb_pause, :kb_home, :kb_up, :kb_pgup, :kb_left, :kb_right, :kb_end, :kb_down, :kb_pgdown, :kb_insert, :kb_delete, :kb_lwin, :kb_rwin, :kb_apps, :kb_power, :kb_sleep, :kb_wake, :kb_websearch, :kb_webfavorites, :kb_webrefresh, :kb_webstop, :kb_webforward, :kb_webback, :kb_mycomputer, :kb_mail, :kb_mediaselect,
+		# There are 145 symbols listed above
+		# That's IDs 0-144 inclusive on both ends
+		
+		# 145
+		nil, 
+		# 146
+		nil, 
+		# 147
+		nil, 
+		# 148
+		nil, 
+		# 149
+		nil, 
+		# 150
+		nil, 
+		# 151
+		nil, 
+		# 152
+		nil, 
+		# 153
+		nil, 
+		# 154
+		nil, 
+		# 155
+		nil, 
+		# 156
+		nil, 
+		# 157
+		:rcontrol, #rcontrol
+		# 158
+		nil, 
+		# 159
+		nil, 
+		# 160
+		nil, 
+		# 161
+		nil, 
+		# 162
+		nil, 
+		# 163
+		nil, 
+		# 164
+		nil, 
+		# 165
+		nil, 
+		# 166
+		nil, 
+		# 167
+		nil, 
+		# 168
+		nil, 
+		# 169
+		nil, 
+		# 170
+		nil, 
+		# 171
+		nil, 
+		# 172
+		nil, 
+		# 173
+		nil, 
+		# 174
+		nil, 
+		# 175
+		nil, 
+		# 176
+		nil, 
+		# 177
+		nil, 
+		# 178
+		nil, 
+		# 179
+		nil, 
+		# 180
+		nil, 
+		# 181
+		nil, 
+		# 182
+		nil, 
+		# 183
+		:kb_printscreen, 
+		# 184
+		:ralt, # ralt
+		# 185
+		nil, 
+		# 186
+		nil, 
+		# 187
+		nil, 
+		# 188
+		nil, 
+		# 189
+		nil, 
+		# 190
+		nil, 
+		# 191
+		nil, 
+		# 192
+		nil, 
+		# 193
+		nil, 
+		# 194
+		nil, 
+		# 195
+		nil, 
+		# 196
+		nil, 
+		# 197
+		nil, 
+		# 198
+		nil, 
+		# 199-208
+		:kb_home, :kb_up, :kb_pgup, nil, :kb_left, :kb_end, :kb_right, :kb_pgdown, :kb_end, :kb_down,
+		# 209
+		:kb_pgdown, 
+		# 210
+		:kb_insert,
+		# 211
+		:kb_delete,
+		# 212
+		nil, 
+		# 213
+		nil, 
+		# 214
+		nil, 
+		# 215
+		nil, 
+		# 216
+		nil, 
+		# 217
+		nil, 
+		# 218
+		nil, 
+		# 219
+		:kb_lwin, 
+		# 220
+		:kb_rwin, 
+		# 221
+		:kb_menu
+	]
+	
+	# def update(dt)
+	# end
+	
+	def setup
+		puts "ruby: Window#setup"
 	end
 	
-	# class Animation
-	# 	class Track
-	# 		def playing?
-	# 			return !ended?
-	# 		end
-	# 	end
-	# end
+	def update
+		puts "ruby: Window#update"
+	end
+	
+	def draw
+		puts "ruby: Window#draw"
+	end
+	
+	def mouse_moved(x,y)
+		p [x,y]
+	end
+	
+	def mouse_pressed(x,y, button)
+		p [:pressed, x,y, button]
+	end
+	
+	def mouse_released(x,y, button)
+		p [:released, x,y, button]
+	end
+	
+	def mouse_dragged(x,y, button)
+		p [:dragged, x,y, button]
+	end
+	
+	
+	
+	private
+	
+	def exit(status_code=0)
+		super(status_code)
+	end
+	
+end
+	
+# class Animation
+# 	class Track
+# 		def playing?
+# 			return !ended?
+# 		end
+# 	end
+# end
 end
--- Oni/lib/test.rb
+++ Oni/lib/test.rb
@@ -1,92 +1,87 @@
-require './oni/oni'
-	
-class Window < Oni::Window
-	def initialize
-		super(self) # pass Ruby instance to C++ land for callbacks, etc
-		
-		puts "ruby: Window#initialize"
-		
-		@p = [0,0]
-		
-		@p_history = Array.new
-		@trail_dt = 1
-	end
-	
-	def setup
-		puts "ruby: Window#setup"
-	end
-	
-	def update
-		# puts "ruby: Window#update"
-		@tick ||= 0
-		@tick += 1
-		if @tick == @trail_dt
-			@tick = 0
-			
-			@p_history << @p
-		end
-		
-		trail_length = 20*3*2
-		# if @tick == 30
-		# 	@p_history.shift
-		# end
-		
-		if @p_history.length > trail_length
-			i = [0, @p_history.length - trail_length - 1].max
-			@p_history.shift(i)
-		end
-		
-		# p @p_history
-	end
-	
-	def draw
-		puts "ruby: Window#draw"
-		z = 1
-		
-		# ofSetColor(255,0,0, 255) # rgba
-		# ofDrawCircle(*@p,z, 20)
-		
-		@p_history.reverse_each.each_with_index do |p, i|
-			next unless i % 3 == 0
-			
-			x,y = p
-			ofSetColor(255,0,0, 255-i*10) # rgba
-			
-			r = 20-i/2
-			r = 0 if r < 0
-			ofDrawCircle(x,y,z, r)
-		end
-	end
-	
-	def mouse_moved(x,y)
-		p [x,y]
-		
-		@p = [x,y]
-	end
-	
-	def mouse_pressed(x,y, button)
-		p [:pressed, x,y, button]
-		
-		exit() if button == 4
-		# TODO: set button codes as constants?
-	end
-	
-	def mouse_released(x,y, button)
-		p [:released, x,y, button]
-	end
-	
-	def mouse_dragged(x,y, button)
-		p [:dragged, x,y, button]
-	end
-	
-	
-	
-	private
-	
-	def exit(status_code=0)
-		super(status_code)
-	end
-end
+require File.expand_path('./oni', File.absolute_path(File.dirname(__FILE__)))
 
-x = Window.new
-x.show
+
+class Window < Oni::Window
+		def initialize
+			super(self) # pass Ruby instance to C++ land for callbacks, etc
+			
+			puts "ruby: Window#initialize"
+			
+			@p = [0,0]
+			
+			@p_history = Array.new
+			@trail_dt = 1
+		end
+		
+		def setup
+			super()
+		end
+		
+		def update
+			super()
+			
+			@tick ||= 0
+			@tick += 1
+			if @tick == @trail_dt
+				@tick = 0
+				
+				@p_history << @p
+			end
+			
+			trail_length = 20*3*2
+			# if @tick == 30
+			# 	@p_history.shift
+			# end
+			
+			if @p_history.length > trail_length
+				i = [0, @p_history.length - trail_length - 1].max
+				@p_history.shift(i)
+			end
+			
+			# p @p_history
+		end
+		
+		def draw
+			super()
+			
+			z = 1
+			
+			# ofSetColor(255,0,0, 255) # rgba
+			# ofDrawCircle(*@p,z, 20)
+			
+			@p_history.reverse_each.each_with_index do |p, i|
+				next unless i % 3 == 0
+				
+				x,y = p
+				ofSetColor(255,0,0, 255-i*10) # rgba
+				
+				r = 20-i/2
+				r = 0 if r < 0
+				ofDrawCircle(x,y,z, r)
+			end
+		end
+		
+		def mouse_moved(x,y)
+			super(x,y)
+			
+			@p = [x,y]
+		end
+		
+		def mouse_pressed(x,y, button)
+			super(x,y, button)
+			
+			exit() if button == 4
+			# TODO: set button codes as constants?
+		end
+		
+		def mouse_released(x,y, button)
+			super(x,y, button)
+		end
+		
+		def mouse_dragged(x,y, button)
+			super(x,y, button)
+		end
+	end
+	
+	x = Window.new
+	x.show
