3df18e0cd20f1ab63f1f1e7d4c04ef1a975d11d5
Raven <RavensKrag@gmail.com>
2016-09-02 00:14:39 -0400

    Update dependencies to the OF_BUILD_VARIABLE_FILE generation task.

--- Oni/build_libs/common.rb
+++ Oni/build_libs/common.rb
@@ -14,6 +14,9 @@ OF_ROOT = "/home/ravenskrag/Experiments/OpenFrameworks/of_v0.9.3_linux64_release
 
 # === these fils should be under the root directory of this Ruby gem
 path_to_file = File.absolute_path(File.dirname(__FILE__))
+
+COMMON_CONFIG = path_to_file
+
 GEM_ROOT = File.expand_path('../', path_to_file)
 
 DYNAMIC_LIB_PATH = File.expand_path("./bin/libs/", GEM_ROOT)
@@ -31,7 +34,10 @@ cpp_root = File.expand_path("ext/#{NAME}/cpp/", GEM_ROOT)
 		OF_SKETCH_SRC_FILES       = Dir.glob(File.join(OF_SKETCH_SRC_DIR, '*{.cpp,.h}'))
 		
 		OF_SKETCH_LIB_OUTPUT_PATH = File.expand_path('lib', OF_SKETCH_ROOT)
-		
+
+
+OF_BUILD_VARIABLE_FILE = File.expand_path("./oF_build_variables.yaml", GEM_ROOT)
+
 
 # === Other build configuration variables
 OF_SKETCH_LIB_FILE = File.join(OF_SKETCH_LIB_OUTPUT_PATH, 'libOFSketch.a')
@@ -42,8 +48,6 @@ NUMBER_OF_CORES    = 4
 
 # TODO: accept platform, target, and number of cores as Rake arguments
 # (maybe you actually want to figure out platform automatially)
-
-OF_BUILD_VARIABLE_FILE = File.expand_path("./oF_build_variables.yaml", GEM_ROOT)
 
 
 
